90167c29a2fd293517f2b9ff1d2f80125a012197
OskiTsukiko <osakitsukiko.mail@gmail.com>
2022-05-26 20:32:36 +0300

    patch focus on click

--- config.h
+++ config.h
@@ -1,13 +1,14 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int gappx     = 7.5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const int focusonwheel       = 0;
+static const char *fonts[]          = { "monospace:size=20" };
+static const char dmenufont[]       = "monospace:size=20";
 static const char col_gray1[]       = "#262b31";
 static const char col_gray2[]       = "#323841";
 static const char col_gray3[]       = "#bbbbbb";
